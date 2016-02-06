6afb8de257f71437fd8e72af3aec12b7d275e3a2
annibal <arthur.annibal@gmail.com>
2016-02-05 22:19:20 -0200

    acho que agora add os bang de volta

--- js/social.js
+++ js/social.js
@@ -1,9 +1,117 @@
 var socialContainer = document.createElement("div");
+socialContainer.setAttribute("style","width:350px; margin:0px auto; margin-top:50px;")
 socialContainer.innerHTML = (
-	"<p style='text-align:left; font:normal small-caps bold 16pt Montserrat,Helvetica,sans-serif;'>Arthur Annibal Tavares</p>\r\n"+
-	"<p style='text-align:left; font:normal small-caps lighter 14pt Montserrat,Helvetica,sans-serif;'>Programmer</p>\r\n"+
-	"<a href='https://www.facebook.com/macannibal' title='' target='_blank' rel='nofollow'><img src='images/facebook.jpg' alt='Facebook' /></a>"+
-	"<a href='https://br.linkedin.com/in/arthur-annibal-24b3478a' title='' target='_blank' rel='nofollow'><img src='images/linkedin.jpg' alt='Linkedin' /></a>"+
-	""
+	"<p style='margin:0px; text-align:left; font:normal small-caps bold 16pt Montserrat,Helvetica,sans-serif;'>"+
+		"Anderson Maciel Carvalho"+
+	"</p>\r\n"+
+	"<p style='margin:0px; margin-bottom:5px; text-align:left; text-transform:lowercase; opacity:.7; font:normal small-caps lighter 14pt Montserrat,Helvetica,sans-serif;'>"+
+		"Artist"+
+	"</p>\r\n"+
+	
+	"<p>\r\n"+
+	"    <img src='images/artist.png'  alt='Artist' width='40' height='40' style='display:inline; float:left; margin:5px;;'/>"+
+	"    <a href='"+
+		"https://www.facebook.com/andersonmaciel.carvalho"+
+	"' title='' target='_blank' rel='nofollow'><img src='images/facebook.png' alt='Facebook' width='40' height='40' style='display:inline; float:right;'/></a>\r\n"+
+	"    <a href='"+
+		"https://www.linkedin.com/in/andycarvalho"+
+	"' title='' target='_blank' rel='nofollow'><img src='images/linkedin.png' alt='Linkedin' width='40' height='40' style='display:inline; float:right;'/></a>\r\n"+
+	"</p>\r\n"+
+	
+	"<div style='float:none; clear:both;'></div>"+
+	"<hr /><br />"+
+	
+	// ----------------
+	
+	"<p style='margin:0px; text-align:left; font:normal small-caps bold 16pt Montserrat,Helvetica,sans-serif;'>"+
+		"Arthur Annibal Tavares"+
+	"</p>\r\n"+
+	"<p style='margin:0px; margin-bottom:5px; text-align:left; text-transform:lowercase; opacity:.7; font:normal small-caps lighter 14pt Montserrat,Helvetica,sans-serif;'>"+
+		"Programmer"+
+	"</p>\r\n"+
+	
+	"<p>\r\n"+
+	"    <img src='images/code.png'  alt='Programmer' width='40' height='40' style='display:inline; float:left; margin:5px;;'/>"+
+	"    <a href='"+
+		"https://www.facebook.com/macannibal"+
+	"' title='' target='_blank' rel='nofollow'><img src='images/facebook.png' alt='Facebook' width='40' height='40' style='display:inline; float:right;'/></a>\r\n"+
+	"    <a href='"+
+		"https://br.linkedin.com/in/arthur-annibal-24b3478a"+
+	"' title='' target='_blank' rel='nofollow'><img src='images/linkedin.png' alt='Linkedin' width='40' height='40' style='display:inline; float:right;'/></a>\r\n"+
+	"</p>\r\n"+
+	
+	"<div style='float:none; clear:both;'></div>"+
+	"<hr /><br />"+
+	
+	// ----------------
+	
+	"<p style='margin:0px; text-align:left; font:normal small-caps bold 16pt Montserrat,Helvetica,sans-serif;'>"+
+		"Geovane dos Santos Medina"+
+	"</p>\r\n"+
+	"<p style='margin:0px; margin-bottom:5px; text-align:left; text-transform:lowercase; opacity:.7; font:normal small-caps lighter 14pt Montserrat,Helvetica,sans-serif;'>"+
+		"Game Designer, Designer and Programmer"+
+	"</p>\r\n"+
+	
+	"<p>\r\n"+
+	"    <img src='images/gamedev.png'  alt='Game Dev' width='40' height='40' style='display:inline; float:left; margin:5px;;'/>"+
+	"    <img src='images/code.png'  alt='Programmer' width='40' height='40' style='display:inline; float:left; margin:5px;;'/>"+
+	"    <a href='"+
+		"https://www.facebook.com/geovane.medina"+
+	"' title='' target='_blank' rel='nofollow'><img src='images/facebook.png' alt='Facebook' width='40' height='40' style='display:inline; float:right;'/></a>\r\n"+
+	"    <a href='"+
+		"https://www.linkedin.com/in/geeomedina"+
+	"' title='' target='_blank' rel='nofollow'><img src='images/linkedin.png' alt='Linkedin' width='40' height='40' style='display:inline; float:right;'/></a>\r\n"+
+	"</p>\r\n"+
+	
+	"<div style='float:none; clear:both;'></div>"+
+	"<hr /><br />"+
+	
+	// ----------------
+	
+	"<p style='margin:0px; text-align:left; font:normal small-caps bold 16pt Montserrat,Helvetica,sans-serif;'>"+
+		"Luiz Felipe Nepomuceno da Silva"+
+	"</p>\r\n"+
+	"<p style='margin:0px; margin-bottom:5px; text-align:left; text-transform:lowercase; opacity:.7; font:normal small-caps lighter 14pt Montserrat,Helvetica,sans-serif;'>"+
+		"Sound Effects"+
+	"</p>\r\n"+
+	
+	"<p>\r\n"+
+	"    <img src='images/sound.png'  alt='Sound Manager' width='40' height='40' style='display:inline; float:left; margin:5px;;'/>"+
+	"    <a href='"+
+		"https://www.facebook.com/LuizBlading"+
+	"' title='' target='_blank' rel='nofollow'><img src='images/facebook.png' alt='Facebook' width='40' height='40' style='display:inline; float:right;'/></a>\r\n"+
+	"    <a href='"+
+		"https://www.linkedin.com/in/luiznepo"+
+	"' title='' target='_blank' rel='nofollow'><img src='images/linkedin.png' alt='Linkedin' width='40' height='40' style='display:inline; float:right;'/></a>\r\n"+
+	"</p>\r\n"+
+	
+	"<div style='float:none; clear:both;'></div>"+
+	"<hr /><br />"+
+	
+	// ----------------
+	
+	"<p style='margin:0px; text-align:left; font:normal small-caps bold 16pt Montserrat,Helvetica,sans-serif;'>"+
+		"Wendell Pereira Barreto da Silva"+
+	"</p>\r\n"+
+	"<p style='margin:0px; margin-bottom:5px; text-align:left; text-transform:lowercase; opacity:.7; font:normal small-caps lighter 14pt Montserrat,Helvetica,sans-serif;'>"+
+		"Game Designer, Sound Effects and Artist"+
+	"</p>\r\n"+
+	
+	"<p>\r\n"+
+	"    <img src='images/artist.png'  alt='Artist' width='40' height='40' style='display:inline; float:left; margin:5px;;'/>"+
+	"    <img src='images/sound.png'  alt='Sound Manager' width='40' height='40' style='display:inline; float:left; margin:5px;;'/>"+
+	"    <img src='images/gamedev.png'  alt='Game Dev' width='40' height='40' style='display:inline; float:left; margin:5px;;'/>"+
+	"    <a href='"+
+		"https://www.facebook.com/wendell.p.b.silva"+
+	"' title='' target='_blank' rel='nofollow'><img src='images/facebook.png' alt='Facebook' width='40' height='40' style='display:inline; float:right;'/></a>\r\n"+
+	"    <a href='"+
+		"https://www.linkedin.com/in/wendell-pereira-barreto-da-silva-83b5518b"+
+	"' title='' target='_blank' rel='nofollow'><img src='images/linkedin.png' alt='Linkedin' width='40' height='40' style='display:inline; float:right;'/></a>\r\n"+
+	"</p>\r\n"+
+	
+	"<div style='float:none; clear:both;'></div>"+
+	"<hr /><br />"
 	
 )
+
+document.body.appendChild(socialContainer);
